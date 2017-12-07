2a9b8bd0ce48b2ecd774b0b5edb5b42d4db1b6e0
LOLO <279144728@qq.com>
2017-12-07 15:32:25 +0800

    updata

--- README.md
+++ README.md
@@ -12,26 +12,41 @@ npm install sequence-fram
 ##### 配置参数
 
 ```
-//以下参数中id,width,height,speed,imgArr必须填写
+//以下参数中带*必须填写
+
 var frame = new SequenceFrame({
-    id: document.getElementBy('id'),    //canvas的DOM节点
-    width: 640,                         //canvas高度
-    height: 1040,                       //canvas宽度
-    speed: 100,                         //绘制的间隔时间
-    imgArr: [],                         //图片路径数组
+    id: document.getElementBy('id'),    //*canvas的DOM节点
+    width: 640,                         //*canvas高度
+    height: 1040,                       //*canvas宽度
+    firstImg: url                       //默认显示的图片
+    imgArr: [],                         //*图片路径数组
+    speed: 100,                         //*绘制的间隔时间
     loop: true,                         //是否循环
-    autoplay: ture,                     //图片加载完后是否播放
+    autoplay: ture,                     //图片加载完后是否播放(默认为true)
     callback: function(){}              //动画结束后的回调函数
+    img_num: function(num,allNum){}     //返回当前帧数，和总帧数
 })
 
 ```
 创建实例后,只有等所有图片加载完毕后才会播放序列帧,可以设置autoplay为false, 等图片加载完毕后,调用play()方法播放序列帧
 
-#### 对外的接口
+### 方法
 ##### play
-停止当前播放的序列帧,从头开始播放
+播放序列帧
 ```
 //创建实力后可调用
 frame.play()
+```
+##### replay
+停止当前播放的序列帧,从头开始播放
+```
+//创建实力后可调用
+frame.replay()
+```
 
+##### pause
+暂停播放(当不使用实力时，调用此方法，停止定时器!!!)
+```
+//创建实力后可调用
+frame.pause()
 ```
--- src/sequenceFrame.js
+++ src/sequenceFrame.js
@@ -4,11 +4,13 @@ function SequenceFrame(options) {
         id: options.id,
         width: options.width,
         height: options.height,
+        firstImg: options.firstImg,
+        imgArr: options.imgArr,
         speed: options.speed,
         loop: options.loop,
-        imgArr: options.imgArr,
         autoplay: options.autoplay,
-        callback: options.callback
+        callback: options.callback,
+        img_num: options.img_num
     };
 
     //初始化参数
@@ -17,6 +19,8 @@ function SequenceFrame(options) {
     this.imgNum = 1;
     this.timer = null;
     this.canvas = this.options.id;
+    this.canvas.width = this.options.width;
+    this.canvas.height = this.options.height;
     this.ctx = this.canvas.getContext('2d');
 
     //加载图片
@@ -33,7 +37,11 @@ SequenceFrame.prototype = {
             this.index++;
             if (this.index > this.options.imgArr.length) {
                 if(this.options.autoplay == undefined || this.options.autoplay){
+                    //是否自动播放
                     this.render();
+                }
+                if(this.options.firstImg){
+                    this.ctx.drawImage(this.source[1], 0, 0, this.options.width, this.options.height);
                 }
             } else {
                 this.imgLoad();
@@ -43,36 +51,50 @@ SequenceFrame.prototype = {
 
     render: function () {
         clearInterval(this.timer);
-        this.timer = setInterval(this.playing.bind(this).bind(this), this.options.speed);
+        this.timer = setInterval(this.playing.bind(this), this.options.speed);
     },
 
     playing: function () {
+        
         //计时器绘图函数
         var imgLength = this.options.imgArr.length;
-        this.canvas.width = this.options.width;
-        this.canvas.height = this.options.height;
-        if (this.imgNum == imgLength && this.options.loop) {
-            //循环播放
-            this.imgNum = 1;
-        } else if (this.imgNum == imgLength && !this.options.loop) {
-            //播放一次后调用回调函数
-            clearInterval(this.timer);
-            if (typeof this.options.callback === 'function') {
-                this.options.callback()
+        if(this.imgNum <= imgLength){
+            if (typeof this.options.img_num === 'function') {
+                this.options.img_num(this.imgNum,imgLength)
             }
+            if (this.imgNum == imgLength && this.options.loop) {
+                //循环播放
+                this.imgNum = 1;
+            }else if(this.imgNum == imgLength && !this.options.loop) {
+                //播放一次后调用回调函数
+                clearInterval(this.timer);
+                if (typeof this.options.callback === 'function') {
+                    this.options.callback()
+                }
+            }
+            //将图片绘制到canvas上
+            this.ctx.clearRect(0, 0, this.options.width, this.options.height);
+            this.ctx.drawImage(this.source[this.imgNum], 0, 0, this.options.width, this.options.height);
         }
-        //将图片绘制到canvas上
-        this.ctx.clearRect(0, 0, this.options.width, this.options.height);
-        this.ctx.drawImage(this.source[this.imgNum], 0, 0, this.options.width, this.options.height);
         this.imgNum++;
     },
 
     //可调用的API
-    play: function () {
+    replay: function(){
         //停止所有播放,手动调用播放
         clearInterval(this.timer);
         this.imgNum = 1;
         this.timer = setInterval(this.playing.bind(this), this.options.speed);
     },
 
+    play: function () {
+        //手动调用播放
+        clearInterval(this.timer);
+        this.timer = setInterval(this.playing.bind(this), this.options.speed);
+    },
+
+    pause: function () {
+        //暂停播放
+        clearInterval(this.timer);
+    },
 }
